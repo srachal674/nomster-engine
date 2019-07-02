3117a6d89ed26319e79e126602357a1e9bc59002
Shannon Rachal <srachal674@gmail.com>
2019-07-02 21:50:34 +0000

    Working on will_paginate error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,5 @@
 class PlacesController < ApplicationController
   
   def index
-    @places = Place.all
-    @places = Place.order("name").page(params[:page]).per_page(10)
+    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 2)
   end
