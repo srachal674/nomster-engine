b493a66bac02640b70e250e3c62b600712eae036
Shannon Rachal <srachal674@gmail.com>
2019-07-04 00:11:40 +0000

    Revert to 0c85cf59a7c622e2589b0bcc1706c9ebfbd13ebc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def index
-    @places = Place.paginate :page => params[:page], :per_page => 50
-  end
 end
-
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,7 @@
 class PlacesController < ApplicationController
+  
+  def index
+    @places = Place.all
+  end
+
 end
