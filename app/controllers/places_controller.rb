2398fec315cdec0fb2dac4c18f9eb274ac7f48c4
Shannon Rachal <srachal674@gmail.com>
2019-07-06 02:21:45 +0000

    Attemp to fix paginate form conflict

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,4 @@
 class PlacesController < ApplicationController
-  
   def index
     @places = Place.all
   end
@@ -8,7 +7,15 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  def new
-    @place = Place.new
+  def create
+    Place.create(place_params)
+    redirect_to root_path    
   end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
