a476cf4cda1aab69281cbfe5f7bf87ffbdf62f42
Shannon Rachal <srachal674@gmail.com>
2019-07-14 20:03:35 +0000

    Update alter_places_add_user_id_column file

--- db/migrate/20190714195809_alter_places_add_user_id_column.rb
+++ db/migrate/20190714195809_alter_places_add_user_id_column.rb
@@ -1,6 +1,6 @@
 class AlterPlacesAddUserIdColumn < ActiveRecord::Migration[5.2]
   def change
-    add_colimn :places. :user_id, :integer
+    add_colimn :places, :user_id, :integer
     add_index :plaes, :user_id
   end
 end
