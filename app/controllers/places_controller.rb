5ca58eea679ed2f163c6210e766d964940a9ad02
Shannon Rachal <srachal674@gmail.com>
2019-07-14 18:30:57 +0000

    Revert to 467f86b4d6436511f2d6e97e2e78253299426c69

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,4 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
   def index
     @places = Place.paginate(:page =>params[:page], per_page: 2)
   end
@@ -10,16 +8,8 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    Place.create(place_params)
     redirect_to root_path    
-  end
-
-  def show
-     @place = Place.find(params[:id])
-  end
-
-  def edit
-    @place = Place.find(params[:id])
   end
 
   private
