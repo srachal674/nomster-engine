f632a1723c29e5bbc4f4c4c92005c2ebdeb920c7
Shannon Rachal <srachal674@gmail.com>
2019-07-16 01:31:58 +0000

    Added geocode

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, length: {minimum: 3}, presence: true
   validates :address, :description, presence: true
 end
