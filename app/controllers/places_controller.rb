3a7d6fbf1cb2e8fd494926d89f5d3fe09689de30
Shannon Rachal <srachal674@gmail.com>
2019-07-16 18:53:23 +0000

    Added geocoder.rb, def address, and activated Google API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -53,6 +53,11 @@ class PlacesController < ApplicationController
       redirect_to root_path
   end
 
+  def address
+    [street, city, state].compact.join(', ')
+    include Geocoder::Model::Mongoid
+  end
+
     private
 
     def place_params
