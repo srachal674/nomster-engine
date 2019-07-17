fc41a9d5e82841f45726db2977da50ea98b69d32
Shannon Rachal <srachal674@gmail.com>
2019-07-17 23:27:31 +0000

    Added function to comment form - Lesson 33

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
@@ -12,5 +12,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:message, :rating)
   end
-
 end
