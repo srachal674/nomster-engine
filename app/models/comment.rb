d250fdf69625c1497c14f5a9a3a9feb71c308ffa
Shannon Rachal <srachal674@gmail.com>
2019-07-17 17:03:33 +0000

    Added User info to models place.rb, user.rb, and comment.rb - Lesson 31

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place 
-  
-    def change
-      create_table :comments do |t|
-        t.text :message
-        t.string :rating
-        t.integer :user_id
-        t.integer :place_id
-        t.timestamps
-    end
-
-    add_index :comments, [:user_id, :place_id]
-    add_index :comments, :place_id
-  end
 end
