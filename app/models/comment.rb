53a168b6f49637b56c1ef57421e3602c9f509b95
Shannon Rachal <srachal674@gmail.com>
2019-07-18 01:21:01 +0000

    Added star ratings

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place 
+
+  Ratings = {
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
+
+  def humanized_rating
+    Ratings.invert[self.rating]
+  end
+
 end
