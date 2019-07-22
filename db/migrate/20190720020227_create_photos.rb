b63c3913bb1e8e82b8600c6dd8e135941b54ec82
Shannon Rachal <srachal674@gmail.com>
2019-07-22 21:26:14 +0000

    Lesson 39 form working w/o pics

--- db/migrate/20190720020227_create_photos.rb
+++ db/migrate/20190720020227_create_photos.rb
@@ -3,7 +3,7 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
       t.text :message
-      t.integer :place_id
+      t.integer :user_id
       t.timestamps
     end
 
