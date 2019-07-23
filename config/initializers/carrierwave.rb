5e454d53cbd627cdb9d9009a5473774f94fad822
Shannon Rachal <srachal674@gmail.com>
2019-07-23 21:15:23 +0000

    Corrected spelling of CarrierWave in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,12 @@
 # config/initializers/carrierwave.rb
 
-#CarrierWave.configure do |config|
- # if Rails.env.development?
- #   config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-  #  config.root = '/home/vagrant/uploads_tmp/tmp'
-  #end
-
-  #config.storage    = :aws
-  #config.aws_bucket = ENV["AWS_BUCKET"]
-  #config.aws_acl    = "public-read"
-
-  #config.aws_credentials = {
-   #   access_key_id:     ENV["AWS_ACCESS_KEY"],
-    # region:            ENV["AWS_REGION"]
-  #}
-#end
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('temp')
-  configure.cache_dir ='carrierwave'
-
-  configure.fog_credentials ={
-    :provider                   =>'AWS'             #requried
-    :aws_access_key    => ENV['AWS_ACCESS_KEY'],       #required
-    :aws_secret_key    => ENV['AWS_SECRET_KEY'],       #required
+  config.fog_provider = 'fog/aws'               #required
+  config.fog_public = false
+  config.fog_credentials = {
+    provider:               'AWS',                  #required
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY'],   #required
+    aws_secret_access_key:  ENV['AWS_SECRET_KEY'],
   }
-  config.fog_directory => ENV['AWS_BUCKET']
-  config.fog_public    = false
+  config.fog_directory = ENV['AWS_BUCKET']
 end
