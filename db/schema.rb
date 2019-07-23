7e11d93be9186a83d35ee5a2312aaa8938b8daa3
Shannon Rachal <srachal674@gmail.com>
2019-07-23 23:02:26 +0000

    DB Reset

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,9 @@ ActiveRecord::Schema.define(version: 2019_07_22_215051) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.text "message"
-    t.string "rating"
-    t.integer "user_id"
+    t.text "caption"
     t.integer "place_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
