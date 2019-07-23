efea1178affb4db11760d377e7b271869460dc4c
Shannon Rachal <srachal674@gmail.com>
2019-07-23 22:37:39 +0000

    Corrected def photo_params in photo_controller

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -4,12 +4,12 @@ class PhotoController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
-    redirect_to 'AWS_BUCKET'
+    redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:picture).permit(:message)
+    params.require(:photo).permit(:picture, :caption)
   end
 end
